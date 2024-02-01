829388e984b66d111ff8cafefa7fb14fd7384854
Jari Aalto <jari.aalto@cante.net>
2024-02-01 20:49:13 +0200

    Update copyright years
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- common.mk
+++ common.mk
@@ -1,6 +1,6 @@
 #!/usr/bin/make -f
 #
-#	Copyright (C) 1997-2009 Jari Aalto
+#	Copyright (C) 1997-2024 Jari Aalto
 #
 #	This program is free software; you can redistribute it and/or
 #	modify it under the terms of the GNU General Public License as
